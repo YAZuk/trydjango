e173b229486da9fd1da710f59d4a94a6d5e57999
Latypov Ramil <r.latypov@mediasoft.team>
2018-09-07 09:28:01 +0400

    changes

--- trydjango/trydjango/settings.py
+++ trydjango/trydjango/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'blog',
     'rest_framework',
     'rest_framework_jwt',
+    'rest_framework.authtoken',
+    'oauth2_provider',
 ]
 
 MIDDLEWARE = [
@@ -144,5 +146,6 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     ),
 }
